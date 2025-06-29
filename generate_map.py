75abd334599cbf66cc3e12918ff3bd567e48be92
Tristan Thomas <tristanpthomas01@gmail.com>
2025-06-29 18:19:15 -0400

    Added import os to python script

--- generate_map.py
+++ generate_map.py
@@ -28,4 +28,5 @@ def serve_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
